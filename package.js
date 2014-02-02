c9995391b31f89ed3663140c42f62397e27944f3
Chris Butler <chris@platformthirteen.com>
2014-02-02 07:27:11 -0500

    update

--- package.js
+++ package.js
@@ -2,11 +2,11 @@ Package.describe({
   summary: "Twitter API Client"
 });
 
-Npm.depends({'twit': '1.1.12'});
+Npm.depends({'twit': '1.1.9'});
 
 Package.on_use(function (api) {
   if(api.export) {
-    api.export('TwitMaker', 'server');
+    api.export('TwitMaker');
   }
   api.add_files('main.js', 'server');
 });
